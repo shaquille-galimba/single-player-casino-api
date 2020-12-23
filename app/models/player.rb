820fcc9a7236dfa2590a4d1f3ea5aa42bda5bbab
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-23 17:53:35 -0500

    fixed routes namespacing, add player model name validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
   belongs_to :game
+	validates :name, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 	# add namespacing for convention
-	namespace: api do
-		namespace: v1 do
+	namespace :api do
+		namespace :v1 do
 			resources :players
 			resources :games
 		end
