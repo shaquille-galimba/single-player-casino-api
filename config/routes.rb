55beabfdbbc3faecae7219e1f220f1e0057e05da
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-23 17:41:58 -0500

    add routes namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :players
-  resources :games
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+	# add namespacing for convention
+	namespace: api do
+		namespace: v1 do
+			resources :players
+			resources :games
+		end
+	end
+
 end
