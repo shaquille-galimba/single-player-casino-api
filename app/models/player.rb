2c655e06d516d4960968307a5967ff3c1920b58d
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-23 19:02:13 -0500

    add score logic

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,23 @@
 class Player < ApplicationRecord
   belongs_to :game
 	validates :name, uniqueness: true
+	before_update :update_scores, :if => :latest_score_changed?
+
+	def update_scores
+		self.update_highest_score_and_profit
+		self.update_game_profit
+	end
+
+	def update_highest_score_and_profit
+		if self.latest_score > self.highest_score
+			self.highest_score = self.latest_score
+		end
+		self.profit += self.latest_score
+	end
+
+	def update_game_profit
+		self.game.profit -= self.latest_score
+		self.game.save
+	end
+	private
 end
