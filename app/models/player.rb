f7d9a75f40b3745c75aa17d632b85fa6998b4dcf
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-23 19:09:35 -0500

    update callback method and make it private

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,23 +1,19 @@
 class Player < ApplicationRecord
   belongs_to :game
 	validates :name, uniqueness: true
-	before_update :update_scores, :if => :latest_score_changed?
+	before_update :update_highest_score_and_profit, :update_game_profit, :if => :latest_score_changed?
 
-	def update_scores
-		self.update_highest_score_and_profit
-		self.update_game_profit
-	end
-
-	def update_highest_score_and_profit
-		if self.latest_score > self.highest_score
-			self.highest_score = self.latest_score
-		end
-		self.profit += self.latest_score
-	end
-
-	def update_game_profit
-		self.game.profit -= self.latest_score
-		self.game.save
-	end
 	private
+		def update_highest_score_and_profit
+			if self.latest_score > self.highest_score
+				self.highest_score = self.latest_score
+			end
+			self.profit += self.latest_score
+		end
+
+		# update game profit according to player's latest earning
+		def update_game_profit
+			self.game.profit -= self.latest_score
+			self.game.save
+		end
 end
