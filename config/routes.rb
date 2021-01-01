e44b871360d30e80dc443004baf9677808d228a4
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2021-01-01 04:39:21 -0500

    configure routes to only show what is used

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 	# add namespacing for convention
 	namespace :api do
 		namespace :v1 do
-			resources :games do
-				resources :players do
+			resources :games, only: [:index, :show] do
+				resources :players, only: [:index, :create, :show, :update] do
 					collection do
 						get :highscores
 					end
